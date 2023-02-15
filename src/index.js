f1b261c7eed7fde2d4bd7e7eb53a0211edc14110
Pâmela Gonçalves <100524022+pamelagonc@users.noreply.github.com>
2023-02-15 10:38:24 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// declarando variavel com let
+
+let numero = 1 
+{
+  let numero = 2
+  console.log('dentro =', numero)
+}
+
+console.log('fora =', numero);
+
+let numero1 = 1
+
+{
+  let numero1 = 2
+  console.log('dentro1 =',numero1)
+}
+
+console.log('fora1 =',numero1)
+
+// declarando uma constante
+
+const numero2 = 2
+
+
+console.log(numero2)
+
